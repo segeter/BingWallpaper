6d1932723680a0b5630d43f4710b69644e983b12
segeter <362174390@qq.com>
2024-09-20 22:47:29 +0800

    global.bing.com

--- main.cpp
+++ main.cpp
@@ -7,62 +7,77 @@
 #include <boost/beast/http/write.hpp>
 #include <boost/beast/ssl/ssl_stream.hpp>
 #include <boost/json/parse.hpp>
-#include <boost/url/parse.hpp>
-#include <boost/url/url.hpp>
+#include <boost/url/url_view.hpp>
 #include <filesystem>
+#include <format>
 #include <fstream>
 #include <iostream>
 #include <vector>
 
+namespace asio = boost::asio;
+namespace urls = boost::urls;
+namespace beast = boost::beast;
+namespace json = boost::json;
+
 // clang-format off
-static constexpr std::string_view kApiUrl = "https://bingwallpaper.microsoft.com/api/BWC/getHPImages?screenWidth=1920&screenHeight=1080&env=live";
+static constexpr std::string_view kApiUrlFormat = "https://global.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&uhd=1&uhdwidth={}&uhdheight={}&setmkt={}&setlang={}";
+static constexpr std::string_view kImgUrlFormat = "https://cn.bing.com{}";
 // clang-format on
 
 template <class T = char>
-std::vector<T> HttpGet(boost::asio::io_context& ioc,
-                       boost::asio::ssl::context& ctx,
-                       const boost::urls::url_view& url) {
+std::vector<T> HttpGet(asio::io_context& ioc, asio::ssl::context& ctx,
+                       const urls::url_view& url) {
   std::string port;
   if (!url.has_port()) {
-    if (url.scheme_id() == boost::urls::scheme::http) {
+    if (url.scheme_id() == urls::scheme::http) {
       port = "80";
-    } else if (url.scheme_id() == boost::urls::scheme::https) {
+    } else if (url.scheme_id() == urls::scheme::https) {
       port = "443";
     } else {
-      throw std::runtime_error("Unknown scheme default port");
+      assert(false);
     }
   } else {
     port = url.port();
   }
   auto host = url.host();
   auto target = url.encoded_target().substr();
-  boost::asio::ip::tcp::resolver resolver{ioc};
-  boost::beast::ssl_stream<boost::beast::tcp_stream> stream{ioc, ctx};
+  asio::ip::tcp::resolver resolver{ioc};
+  beast::ssl_stream<beast::tcp_stream> stream{ioc, ctx};
   auto results = resolver.resolve(host, port);
-  boost::beast::get_lowest_layer(stream).connect(results);
-  stream.handshake(boost::asio::ssl::stream_base::client);
-  boost::beast::http::request<boost::beast::http::empty_body> req{
-      boost::beast::http::verb::get, target, 11};
-  req.set(boost::beast::http::field::host, host);
-  boost::beast::http::write(stream, req);
-  boost::beast::flat_buffer buffer;
-  boost::beast::http::response<boost::beast::http::vector_body<T>> res;
-  boost::beast::http::read(stream, buffer, res);
+  beast::get_lowest_layer(stream).connect(results);
+  stream.handshake(asio::ssl::stream_base::client);
+  beast::http::request<beast::http::empty_body> req{beast::http::verb::get,
+                                                    target, 11};
+  req.set(beast::http::field::host, host);
+  beast::http::write(stream, req);
+  beast::flat_buffer buffer;
+  beast::http::response<beast::http::vector_body<T>> res;
+  beast::http::read(stream, buffer, res);
   return std::move(res.body());
 }
 
-void SetWorkDirectory() {
-  wchar_t path[MAX_PATH];
-  GetTempPath(MAX_PATH, path);
-  SetCurrentDirectory(path);
+struct ScreenResolution {
+  std::int32_t width = 0;
+  std::int32_t height = 0;
+};
+
+ScreenResolution GetScreenResolution() noexcept {
+  std::int32_t width = GetSystemMetrics(SM_CXSCREEN);
+  std::int32_t height = GetSystemMetrics(SM_CYSCREEN);
+  return {.width = width, .height = height};
 }
 
-std::vector<char> DownloadImage(boost::asio::io_context& ioc,
-                                boost::asio::ssl::context& ctx,
-                                std::string_view url) {
-  auto data = HttpGet(ioc, ctx, url);
-  auto jv = boost::json::parse({data.data(), data.size()});
-  auto img_url = jv.at("images").as_array().at(0).at("url").as_string();
+std::vector<char> DownloadImage(asio::io_context& ioc, asio::ssl::context& ctx,
+                                ScreenResolution resolution,
+                                std::string_view lcid) {
+  auto url = std::format(kApiUrlFormat, resolution.width, resolution.height,
+                         lcid, lcid);
+  std::cout << "GET " << url << std::endl;
+  auto js = HttpGet(ioc, ctx, url);
+  auto jv = json::parse({js.data(), js.size()});
+  auto img_path = jv.at("images").as_array().at(0).at("url").as_string();
+  auto img_url = std::format(kImgUrlFormat, img_path.c_str());
+  std::cout << "GET " << img_url << std::endl;
   return HttpGet(ioc, ctx, img_url);
 }
 
@@ -72,6 +87,7 @@ std::filesystem::path SaveImage(const std::vector<char>& data) {
     std::filesystem::create_directories(img_path);
   }
   img_path.append("BingWallpaper.jpg");
+  std::cout << "SAVE " << img_path << std::endl;
   std::ofstream file(img_path, std::ios_base::out | std::ios_base::binary);
   if (!file.is_open()) {
     throw std::runtime_error("Cannot open file: " + img_path.string());
@@ -82,6 +98,7 @@ std::filesystem::path SaveImage(const std::vector<char>& data) {
 }
 
 void SetDesktopWallpaper(const std::filesystem::path& img_path) {
+  std::cout << "SET " << img_path << std::endl;
   bool status = SystemParametersInfo(
       SPI_SETDESKWALLPAPER, 0, (PVOID)img_path.c_str(), SPIF_UPDATEINIFILE);
   if (!status) {
@@ -91,18 +108,20 @@ void SetDesktopWallpaper(const std::filesystem::path& img_path) {
 }
 
 int main(int argc, char* argv[]) {
-  boost::asio::io_context ioc{1};
-  boost::asio::ssl::context ctx(boost::asio::ssl::context::tlsv12_client);
-  ctx.set_verify_mode(boost::asio::ssl::verify_none);
+  std::string lcid = "en-us";
+  if (argc > 1) {
+    lcid = argv[1];
+  }
+
+  asio::io_context ioc{1};
+  asio::ssl::context ctx(asio::ssl::context::tlsv12_client);
+  ctx.set_verify_mode(asio::ssl::verify_none);
+
   try {
-    SetWorkDirectory();
-    std::cout << "Download image..." << std::endl;
-    auto img_data = DownloadImage(ioc, ctx, kApiUrl);
-    std::cout << "Save image..." << std::endl;
+    auto res = GetScreenResolution();
+    auto img_data = DownloadImage(ioc, ctx, res, lcid);
     auto img_path = SaveImage(img_data);
-    std::cout << "Set desktop wallpaper..." << std::endl;
     SetDesktopWallpaper(img_path);
-    std::cout << "Completed" << std::endl;
   } catch (const std::exception& e) {
     std::cout << e.what() << std::endl;
     system("pause");
--- x64/Release/ScheduledTask.ps1
+++ x64/Release/ScheduledTask.ps1
@@ -17,13 +17,15 @@ if ("register" -ieq $Arg)
 {
 	$Description = "Update Bing wallpapers daily"
 	$FilePath = (Get-Location).path + "\BingWallpaper.exe"
-	$Action = New-ScheduledTaskAction -Execute "$FilePath"
+	$Action = New-ScheduledTaskAction -Execute "$FilePath" -Argument en-us -WorkingDirectory %temp%
 	$Trigger = New-ScheduledTaskTrigger -Daily -At 4pm
 	$Settings = New-ScheduledTaskSettingsSet -StartWhenAvailable -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 	$Task = New-ScheduledTask -Description $Description -Action $Action -Trigger $Trigger -Settings $Settings
 	Register-ScheduledTask -TaskName $TaskName -InputObject $Task
+	Start-ScheduledTask -TaskName $TaskName
 }
 else
 {
+	Stop-ScheduledTask -TaskName $TaskName
 	Unregister-ScheduledTask -TaskName $TaskName -Confirm:$false
 }
